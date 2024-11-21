fdf94a8e33e95216a51cf3fb18f98930e498d5d5
demon192 <ishumehra1534@gmail.com>
2024-11-21 11:29:46 +0530

    backend_5

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -109,7 +109,7 @@ const Add = ({url}) => {
               value={data.price}
               type="Number"
               name="price"
-              placeholder="$20"
+              placeholder="Rs. 200"
             />
           </div>
         </div>
